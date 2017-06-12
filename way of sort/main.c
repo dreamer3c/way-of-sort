1ea28a337dbe6fa248a317102e77c0247a1efec0
dreamer3c <2978425474@qq.com>
2017-06-12 09:59:59 +0800

    dreamer3c
    File and sort

---
+++ way of sort/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  way of sort
+//
+//  Created by 20161104579 on 2017/6/12.
+//  Copyright © 2017年 20161104579. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
