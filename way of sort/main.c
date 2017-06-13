155489632080fb70879f6dc720cab13ce3d68a66
dreamer3c <2978425474@qq.com>
2017-06-13 11:07:36 +0800

    dreamer3c
    new

--- way of sort/main.c
+++ way of sort/main.c
@@ -8,7 +8,40 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-
-    return 0;
+int main()
+{
+    FILE *fp1,*fp2;
+    int a[10],i,j,t;
+    fp1=fopen("list.txt","r");
+    if(fp1==NULL)
+    {
+        printf("file not exist");
+    }
+    else
+    {
+        for(i=0;i<10;i++)
+        {
+            fscanf(fp1,"%d",&a[i]);
+        }
+        fclose(fp1);
+        for(i=0;i<10;i++)
+        {
+            for(j=1;j<10-i;j++)
+            {
+                if(a[i]>a[i+j])
+                {
+                    t=a[i];
+                    a[i]=a[i+j];
+                    a[i+j]=t;
+                }
+            }
+        }
+        for(i=0;i<10;i++)
+        {
+            fp2=fopen("list2.txt","w");
+            fputc(a[i],fp2);
+        }
+        fclose(fp2);
+    }
+        return 0;
 }
